3f6ec8009975afec85e041fc59f5af659251df58
SamiraCemoi <samiracommeca@gmail.com>
2024-07-08 22:11:24 +0200

    Update Calculatrice.cs

--- Calculatrice.cs
+++ Calculatrice.cs
@@ -1,16 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-using System;     // directive essentielle qui permet d'importer le namespace qui contient des classes et va faciliter l'utilisation de celles ci dans namespace
 
 
-namespace Calculatrice  // je crée ma boîte générale qui regroupe les classes et structures 
-{
-    class program // classe qui est le point d'entree de mon programme
-    {
-        static void Main(string[] args) // static veut dire que la méthode appartient à la classe et pas aux objets qui sont dans la classe
-            // nom de la méthode qui est executée + paramètre de la méthode
-        {
             int res;
             
 
